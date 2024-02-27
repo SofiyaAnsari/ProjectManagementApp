e0d07e688c55ba74218a63f87b75084a2f6ac0ac
Sofiya Ansari <sofiyaansari972@gmail.com>
2024-02-27 01:53:31 -0500

    first commit

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+export default function NewTask({onAdd}){
+   const [enteredTask,setEnteredTask]= useState('');
+   function handleChange(event){
+    setEnteredTask(event.target.value);
+   }
+   function handleClick(){
+if(enteredTask.trim()===''){
+    return;
+}setEnteredTask('');
+onAdd(enteredTask);
+   }
+    return (<div className="flex items-center gap-4">
+        <input type="text" onChange={handleChange} value={enteredTask} className="bg-stone-200 w-64 px-2 py-1 rounded-sm"/>
+        <button onClick={handleClick} className="text-stone-700 hover:text-stone-950">Add Task</button>
+    </div>)
+}
